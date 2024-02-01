c650706e05034acaddb61ff0b084982e524490d9
frederic <flausson@normandiewebschool.fr>
2024-02-01 15:23:00 +0100

    mvp

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -4,8 +4,17 @@ attribute vec3 position;
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
 
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+
 // uniform float Utime;
 
 void main() {
-	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+	// transform => position, scale and rotation
+	// modelMatrix => position, scale and rotation of model
+	// wiewMatrix => position, orientation of caméra
+	// projectionMatrix => project our object into screan ( aspect ratio and perspective)
+
+	// MVP ( order maters)
+	// gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
 }
